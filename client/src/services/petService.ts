e35c1b6b8f8d623c2bdf10602aae610236f9966c
Wanfredo Vargas <84217875+wanfrev@users.noreply.github.com>
2026-07-20 20:22:19 -0400

    Update petService.ts

--- client/src/services/petService.ts
+++ client/src/services/petService.ts
@@ -1,4 +1,4 @@
-import { db } from '../lib/api'
+import { apiRequest } from '../lib/api'
 import type { Pet } from '../types/database'
 
 export const petsKeys = {
@@ -6,23 +6,5 @@ export const petsKeys = {
 }
 
 export const listPetsByClient = async (clientId: string): Promise<Pet[]> => {
-  const { data, error } = await db
-    .from('pets')
-    .select('*')
-    .eq('client_id', clientId)
-    .order('name')
-
-  if (error) throw error
-  return data as Pet[]
-}
-
-export const getPetById = async (petId: string): Promise<Pet> => {
-  const { data, error } = await db
-    .from('pets')
-    .select('*')
-    .eq('id', petId)
-    .single()
-
-  if (error) throw error
-  return data as Pet
+  return apiRequest<Pet[]>('GET', `/clients/${clientId}/pets`)
 }
