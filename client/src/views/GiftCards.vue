c56d712d47ab820c3f54888166c544c4f0f09b44
Adriana Montiel <adrianakmontiele@gmail.com>
2026-07-26 13:30:18 -0400

    ya

--- client/src/views/GiftCards.vue
+++ client/src/views/GiftCards.vue
@@ -26,11 +26,6 @@
           Nueva Gift Card
         </button>
       </div>
-        <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
-        </svg>
-        Nueva Gift Card
-      </button>
     </div>
   </header>
 
