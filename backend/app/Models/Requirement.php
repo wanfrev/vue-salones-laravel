72730fd144eb91b6332323e16a47a10db8111b8d
Adriana Montiel <adriana.29842525@uru.edu>
2026-08-08 18:21:11 -0400

    Update Requirement.php

--- backend/app/Models/Requirement.php
+++ backend/app/Models/Requirement.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Concerns\GeneratesUuid;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Requirement extends Model
 {
-    use HasFactory, SoftDeletes, GeneratesUuid;
+    use HasFactory, SoftDeletes, HasUuids;
 
     public $incrementing = false;
     protected $keyType = 'string';
